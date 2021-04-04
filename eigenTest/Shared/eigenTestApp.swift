1a2d85954afbd7a0088f7393e01238291036d758
Zongyao QU <zongyao.qu@gmail.com>
2021-04-04 16:46:17 +0900

    [GEN] add basic test project

---
+++ eigenTest/Shared/eigenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  eigenTestApp.swift
+//  Shared
+//
+//  Created by NILTSH on 2021/4/4.
+//
+
+import SwiftUI
+
+@main
+struct eigenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
